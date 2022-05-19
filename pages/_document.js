83e0ff83e6df4eb35530c7642ea12d2508fe29e7
caner ozkan <caner@favorisanat.com>
2022-05-19 17:58:47 +0300

    document.js and gtm scripts added

---
+++ pages/_document.js
@@ -0,0 +1,21 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P4XLRM8"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            }}
+          ></noscript>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -35,3 +35,4 @@ const NewMeetup = (props) => {
 };
 
 export default NewMeetup;
+
